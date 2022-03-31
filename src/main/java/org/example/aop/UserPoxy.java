16c3e3aee1d183772b17196cc585b4bb37e266be
shy <2759643667@qq.com>
2022-03-31 21:52:10 +0800

    modify UserPoxy

--- src/main/java/org/example/aop/UserPoxy.java
+++ src/main/java/org/example/aop/UserPoxy.java
@@ -1,7 +1,7 @@
 package org.example.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +9,24 @@ import org.springframework.stereotype.Component;
 @Aspect
 
 public class UserPoxy {
+    //前置执行方法
     @Before(value = "execution(* *..User.doSome(..))")
     public void before(){
         System.out.println("before...");
+    }
+    //后置执行方法
+    @After(value = "execution(* org.example.aop.User.doSome())")
+    public void after(){
+        System.out.println("after...");
+    }
+    @AfterReturning(value = "execution(* org.example.aop.User.doSome())")
+    public void AfterReturning(){
+        System.out.println("afterReturning...");
+    }
+    @Around(value = "execution(* org.example.aop.User.doSome())")
+    public void Around(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Around...前");
+        pjp.proceed();
+        System.out.println("Around...后");
     }
 }
